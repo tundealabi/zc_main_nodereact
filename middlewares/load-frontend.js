8c547a820d87c2b297a18cb44df03b846decaa32
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-08-29 15:51:19 +0100

    Update to redirect to frontend developement server on dev mode

--- middlewares/load-frontend.js
+++ middlewares/load-frontend.js
@@ -7,9 +7,7 @@ module.exports = (app) => {
   app.use(
     (_, res, next) => {
       if (!isProduction) {
-        return res.send(
-          `This is development build <a href="http://127.0.0.1:3000">View Frontend</a>`
-        )
+        return res.redirect('http://127.0.0.1:3000') // redirect to frontend in dev build
       }
       next()
     }, // below this are for production
